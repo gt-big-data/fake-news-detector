56d63191d77ba299e010676398e44d8b3504167b
Ha Dang <danghoangha1996@gmail.com>
2020-10-31 16:29:52 -0400

    Modify npr-parser

--- platform/npr-parser/NPRParser.py
+++ platform/npr-parser/NPRParser.py
@@ -1,40 +1,41 @@
 # NPR Parser
-import requests;
-import urllib.request;
-import time;
-import bs4;
-from bs4 import BeautifulSoup;
+import requests
+import urllib.request
+import time
+import bs4
+from bs4 import BeautifulSoup
 
 def NPR_article_parser(url):
-    output = {};
+    output = {}
 
-    response = requests.get(url);
-    soup = BeautifulSoup(response.text, "html.parser");
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    article = soup.find("article", class_="story")
 
     # the article title is wrapped within the h1 tag inside a div with class="storytitle"
-    title_div = soup.find("div", class_="storytitle");
-    h1_tag = title_div.find("h1");
-    output["title"] = h1_tag.string;
+    h1_tag = article.find("div", class_="storytitle").h1
+    output["title"] = h1_tag.string
 
-    # the article body is wrapped within <p> tags and <em> tags
-    output["body"] = "";
-    content_tags = soup.find_all("p");
+    # the article body is wrapped within <p> tags
+    output["body"] = ""
+    content_tags = article.find_all("p")
     for tag in content_tags:
-        print([x for x in tag.children]);
-        output["body"] += find_text(tag);
+        output["body"] += find_text(tag)
 
-    return output;
+    return output
 
 def find_text(tag):
-    if tag.name == None:
-        return tag.strip() + " ";
-    else:
-        text = "";
-        for child in tag.children:
-            text += find_text(child);
-        return text;
+    paragraph = ""
+    for element in tag.children:
+        # The find_all method return an image caption twice. One of the repeats contains <b>hide caption<b> tag
+        # Ignore that tag if we see it.
+        if element.string.strip() == "hide caption":
+            return " "
+        # for all other child elements, extract the text and append it to paragraph
+        paragraph += element.string.strip() + " "
+    return paragraph
 
 if __name__ == "__main__":
-    url = input().strip();
-    result = NPR_article_parser(url);
-    print(result);
+    url = input().strip()
+    result = NPR_article_parser(url)
+    print(result)
